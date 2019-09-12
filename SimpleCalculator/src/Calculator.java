7f35367a9b8874666dbd589fe8771ccdcc489144
mw123878 <43249155+mw123878@users.noreply.github.com>
2019-09-12 13:01:41 -0700

    Initial files

---
+++ SimpleCalculator/src/Calculator.java
@@ -0,0 +1,18 @@
+
+public class Calculator {
+	public static void main(String[] args) {
+		
+	}
+	
+	public int plus(int n) {
+	}
+	
+	public int minus(int n) {
+	}
+	
+	public int divide(int n) {
+	}
+	
+	public int multiply(int n) {
+	}
+}
