9ecdc102abd89003e22154b5ade14df487330d6b
hmehain <30275231+hmehain@users.noreply.github.com>
2019-09-19 11:37:32 -0700

    made a mistake in update calculator

--- SimpleCalculator/src/Calculator.java
+++ SimpleCalculator/src/Calculator.java
@@ -9,9 +9,9 @@ public class Calculator {
 	
 	public int minus(int n, int m) {
 	}
-	
+	/*Is this how you're supposed to divide*/
 	public int divide(int n, int m) {
-		return n/m;
+		return m/n;
 	}
 	
 	public int multiply(int n, int m) {
